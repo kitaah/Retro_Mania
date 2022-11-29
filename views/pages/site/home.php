30c432d8793970d11c2df73dc8b63331df9abdcd
John Doe <malachipito@protonmail.com>
2022-11-29 01:26:19 +0100

    Correctif

--- views/pages/site/home.php
+++ views/pages/site/home.php
@@ -1,9 +1,7 @@
 <?php ?>
 <main>
-    <section class="site-home__container">
-        <article class="site-home-top text-center">
-            <h1>Retrowave Cap Brand</h1>
-            <a href="index.php?page=caps_catalog" id="site-home-top__link" class="flex-center margin-auto">Check the caps now ⟶</a>
-        </article>
-    </section>
+    <article class="site-home__container site-home-top text-center">
+        <h1>Retrowave Cap Brand</h1>
+        <a href="index.php?page=caps_catalog" id="site-home-top__link" class="flex-center margin-auto">Check the caps now ⟶</a>
+    </article>
 </main>
